2090b6fda7b984f0a372b510164247430c0630fc
Prerna chauhan <preranachauhan742@gmail.com>
2023-10-18 15:20:04 +0530

    DAY13-14

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <nav>
+  <ul>
+    <h3>
+      <li>HOME</li>
+      <li>ABOUT US</li>
+      <li>CONTACT US</li>
+      <li>BLOGS</li>
+      <li>GALLERY</li>
+    </h3>
+  </ul>
+  <div className="main">
+    <div className="head">
+      <div className="body">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>
+          HI I'M PRERNA CHAUHAN
+          <br />I AM FULL STACK WEB DEVELOPER
+        </h1>
+      </div>
+    </div>
+  </div>
+  <img src="Prernachauhan.jpeg" alt="" />
+</nav>
+
+    
+    
+    
     </div>
   );
 }
